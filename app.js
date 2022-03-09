e9d72425e6a9d19f444c6ebb12c0b2e978c7b387
M Fakhri Darmawan <fakhridarmawan@gmail.com>
2022-03-09 11:20:01 +0700

    bug fixing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World from github'))
+app.get('/', (req, res) => res.send('Bug fixing'))
 app.listen(3000, () => console.log('Server ready'))
