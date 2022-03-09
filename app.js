99800a757e0d28c21c3b965048eaf3ed99389bf6
M Fakhri Darmawan <fakhridarmawan@gmail.com>
2022-03-09 10:49:47 +0700

    fix bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello CI/CD!'))
+app.get('/', (req, res) => res.send('Hello World from github'))
 app.listen(3000, () => console.log('Server ready'))
