eced8218de7789e396f4b53d50cd4b8c79cf8ab0
fakhridarmawan <fakhridarmawan@gmail.com>
2022-08-18 11:32:30 +0700

    demo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello ACR !!'))
+app.get('/', (req, res) => res.send('Hello ACR Github!!'))
 app.listen(3000, () => console.log('Server ready'))
